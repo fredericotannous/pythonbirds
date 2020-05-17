fca9f5f306d6fa9b7a86e5fac4cb84a134d900db
Frederico Tannous <62360313+fredericotannous@users.noreply.github.com>
2020-05-17 19:00:44 +0200

    exercício: criando um carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,13 +6,21 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
 
-    def cumprimentar(self):
+    def cumprimentar(self): #MÉTODO
         return f'olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls): #quando você quer acessar dados da própria classe
+        return f'{cls} - olhos {cls.olhos}'
 
 
 if __name__ == '__main__':
     mister = Pessoa(nome = 'Mister')
-    luciano = Pessoa(mister, nome ='Luciano')
+    luciano = Pessoa(mister, nome ='Luciano') #objeto
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -29,3 +37,5 @@ if __name__ == '__main__':
     print(mister.olhos)
     print(luciano.olhos)
     print(id(mister.olhos), id(luciano.olhos), id(Pessoa.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
